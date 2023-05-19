1b71ceee5e3ffbb28b7a0095327a1d6e36c94ab8
Victor Italo <victoritalo@live.com>
2023-05-18 23:54:48 -0300

    :sparkles: feat: Adicionando calculo matematico

--- calculadoraIMC-BMI/index.js
+++ calculadoraIMC-BMI/index.js
@@ -13,6 +13,31 @@ function calcular_imc() {
     window.location.reload()
     return;
   }
+
+  let imc = peso / (altura / 100) ** 2;
+  imc = imc.toFixed(2);
+  if (imc < 18.5) {
+    console.log(imc);
+    valorResult.innerHTML = imc;
+    grauResultado.innerHTML = "Abaixo do peso ideal";
+    calcResultStyle.setAttribute("style", "display: block");
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    valorResult.innerHTML = imc;
+    grauResultado.innerHTML = "Peso normal ou adequado";
+    calcResultStyle.setAttribute("style", "display: block");
+  } else if (imc >= 30.0 && imc <= 34.9) {
+    valorResult.innerHTML = imc;
+    grauResultado.innerHTML = "Obesidade Grau I";
+    calcResultStyle.setAttribute("style", "display: block");
+  } else if (imc >= 35.0 && imc <= 39.9) {
+    valorResult.innerHTML = imc;
+    grauResultado.innerHTML = "Obesidade Grau II";
+    calcResultStyle.setAttribute("style", "display: block");
+  } else if (imc >= 40) {
+    valorResult.innerHTML = imc;
+    grauResultado.innerHTML = "Obesidade Grau III ou Mórbida";
+    calcResultStyle.setAttribute("style", "display: block");
+  }
 }
 calcularBtn.addEventListener("click", () => {
   calcular_imc();
